2c87339e9eea7373cbe1c7862d0a0895d98cec0a
Linux Nattawat <68011212094@msu.ac.th>
2025-07-16 19:41:08 +0700

    fix encoding

---
+++ Learning_16_07_68.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+int main()
+
+{
+
+/* {    //Program คำนวณหาจำนวนข้าวสารที่เหลือในถุงและจำนวนถุง
+        float Rice,Bag,Rice_left;
+
+        //
+        printf("\n Input Rice :");
+        scanf("%f",&Rice);
+
+        Rice_left = (int)Rice  %10;
+        Bag = (Rice-Rice_left)/10;
+
+        printf("\n Bag : %.0f\n",Bag);
+        printf("\n Rice_Left : %.0f\n",Rice_left);
+
+    }*/
+
+
+/* {
+        float Bonus,mom,child;    //Data Type & Vairable
+
+        //Input
+        printf("\n Dad_Bonus ;");
+        scanf("%f",&Bonus);     //Input Dad_Bonus
+
+        //Process
+        mom = Bonus*30/100;     //Process Bonus Share To Mom
+        child = (Bonus-mom)/3;      //Process Bonus Share To Child
+
+        //Output
+        printf("\n Share To Mom : %.2f\n",mom);     //Output Share To Mom
+        printf("\n Share To Child : %d\n",3%3);     //Output Share To Child
+
+        return 0;
+    }*/
+
+
+/* {    //Program คำนวณภาษี
+        float salary,Tax,Balance;    //Data Type&Variable
+
+        //Input
+        printf("\nSalary :");    //Print Info For User
+        scanf("%f",&salary);       //Input Salary
+
+        //Process
+        Tax = salary * 3 / 100; //Process Tax
+        Balance = salary - Tax; //Process Balance
+
+        //Output
+        printf("\nTax :%0.2f\n",Tax);    //Output Tax
+        printf("\nBalance :%.2f\n",Balance);     //Output Balance
+
+        return 0; //END
+
+    }*/
+
+
+/* {    //Program Calculator Area Triangle
+        int base,height;
+        float area;
+
+        printf("\nInput Base :");
+        scanf("%d",&base);   //Input Base
+        printf("\nInput Height :");
+        scanf("%d",&height);     //Input Height
+
+        area =(float)1/2*base*height;    //Process 1/2*Base*Height
+
+        printf("\nArea = %.2f\n",area); //Output Area
+
+        return 0;
+    }*/
+
+
+/* {    //Program คำนวณจำนวนเงินที่ได้รับ
+
+
+        int num;    //ตัวแปรจำนวนสินค้า
+        float price;    // ตัวแปรราคาสินค้า
+        float Rs;    //ตัวแปรผลลัพธ์
+
+        printf("\nใส่จำนวนสินค้า :");
+        scanf("%d",&num);    //รับ Input จำนวนสินค้า
+        printf("\nใส่ราคาสินค้าต่อหน่วย :");
+        scanf("%f",&price);      //รับ Input ราคาสินค้า
+
+        Rs = num*price;     //Process จำนวนสินค้า * ราคาสินค้า
+
+        printf("\nราคาสินค้ารวม : %.2f\n บาท",Rs); //Output ราคาสินค้ารวม
+        return 0;
+
+    }*/
+}
